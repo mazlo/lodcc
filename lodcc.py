95d8374d5473f5aa1d8048ede68c8fe4574e42d8
Matthäus Zloch <matthaeus.zloch@gmail.com>
2017-11-24 17:45:48 +0100

    add db connection. use psycopg2 library and use their naming conventions

--- lodcc.py
+++ lodcc.py
@@ -11,30 +11,31 @@
 import re
 import os
 import argparse
+import psycopg2
 
-def ensure_db_schema_complete( r, curr ):
+def ensure_db_schema_complete( r, cur ):
     ```ensure_db_schema_complete```
 
-    curr.execute( "SELECT column_name FROM information_schema.columns WHERE table_name = 'stats';" )
+    cur.execute( "SELECT column_name FROM information_schema.columns WHERE table_name = 'stats';" )
 
     if not r['format']:
         # TODO create error message and exit
         return False;
 
     attr = re.sub( r'[+/]', '_', r['format'] )
-    if attr not in curr.fetchall():
-        curr.execute( "ALTER TABLE stats ADD COLUMN '"+ attr +"' varchar;" )
+    if attr not in cur.fetchall():
+        cur.execute( "ALTER TABLE stats ADD COLUMN '"+ attr +"' varchar;" )
 
     return attr
 
-def save_value( curr, datahub_url, attribute, value, check=True ):
+def save_value( cur, datahub_url, attribute, value, check=True ):
     ```save_value```
 
     if check and not value:
         # TODO create warning message
         print 'no value for attribute '+ attribute +'. could not save'
     else:
-        curr.execute( 'UPDATE stats SET '+ attribute +'="'+ value +'" WHERE url = "'+ datahub_url +'";' )
+        cur.execute( 'UPDATE stats SET '+ attribute +'="'+ value +'" WHERE url = "'+ datahub_url +'";' )
 
 def parse_resource_urls( datahub_url, dry_run=False ):
     ```parse_resource_urls```
@@ -52,17 +53,17 @@ def parse_resource_urls( datahub_url, dry_run=False ):
                 return None
 
             for r in dp['resources']:
-                attr = ensure_db_schema_complete( r, curr )
+                attr = ensure_db_schema_complete( r, cur )
 
                 if not attr:
                     continue
 
-                save_value( curr, datahub_url, dp, attr, r['url'], False )
+                save_value( cur, datahub_url, dp, attr, r['url'], False )
 
-            save_value( curr, datahub_url, 'name', dp['name'] if 'name' in dp else None )
-            save_value( curr, datahub_url, 'keywords', dp['keywords'] if 'keywords' in dp else None )
+            save_value( cur, datahub_url, 'name', dp['name'] if 'name' in dp else None )
+            save_value( cur, datahub_url, 'keywords', dp['keywords'] if 'keywords' in dp else None )
             # save whole datapackage.json in column
-            save_value( curr, datahub_url, 'datapackage_content', str( json.dumps( dp ) ), False )
+            save_value( cur, datahub_url, 'datapackage_content', str( json.dumps( dp ) ), False )
 
         except:
             # TODO create error message and exit
@@ -90,8 +91,8 @@ def save_stats( stats, sid ):
 
 #dry_run = True
 #
-#curr.execute( 'SELECT id,url,format FROM stats' + ';' if not dry_run else ' WHERE domain="Cross_domain" AND title LIKE "%Museum%";' )
-#datasets = curr.fetchall()
+#cur.execute( 'SELECT id,url,format FROM stats' + ';' if not dry_run else ' WHERE domain="Cross_domain" AND title LIKE "%Museum%";' )
+#datasets = cur.fetchall()
 #
 #for ds in datasets:
 #    
@@ -122,24 +123,41 @@ if __name__ == '__main__':
         with open( 'db.properties', 'rt' ) as f:
             args = dict( ( key.replace( '.', '-' ), value ) for key, value in ( re.split( "=", option ) for option in ( line.strip() for line in f ) ) )
     else:
-        parser.add_argument( '--db-hostname', '-H', action = "store", type = str, help = '', default = "localhost" )
-        parser.add_argument( '--db-username', '-U', action = "store", type = str, help = '', default = "zlochms" )
+        parser.add_argument( '--db-host', '-H', action = "store", type = str, help = '', default = "localhost" )
+        parser.add_argument( '--db-user', '-U', action = "store", type = str, help = '', default = "zlochms" )
         parser.add_argument( '--db-password', '-P', action = "store", type = str, help = '', default = "zlochms" )
-        parser.add_argument( '--db-schema', '-S', action = "store", type = str, help = '', default = "zlochms" )
+        parser.add_argument( '--db-dbname', '-S', action = "store", type = str, help = '', default = "zlochms" )
 
     argsps = parser.parse_args()
 
-    for arg in ['dry_run', 'db-hostname', 'db-username', 'db-password', 'db-schema']:
+    for arg in ['dry_run', 'db-host', 'db-user', 'db-password', 'db-dbname']:
         if not arg in args:
             args[arg] = getattr( argsps, arg )
 
-    if "dry_run" in args:
-        print "Running in dry-run mode"
-    else:
-        if "parse-resource-urls" in argsps:
-            parse_resource_urls( 'https://old.datahub.io/dataset/museums-in-italy', argsps['dry-run'] )
+    # connect to an existing database
+    conn = psycopg2.connect( host=args['db-host'], dbname=args['db-dbname'], user=args['db-user'], password=args['db-password'] )
+    cur = conn.cursor()
+
+    try:
+        cur.execute( "SELECT 1;" )
+        result = cur.fetchall()
+    except:
+        print "Database not ready for query execution. "+ sys.exc_info()[0]
+        raise 
+
+    # option 1
+    if "parse_resource_urls" in argsps:
+        if "dry_run" in args:
+            print "Running in dry-run mode"
+            print "Using example dataset 'Museums in Italy'"
+    
+            parse_resource_urls( 'https://old.datahub.io/dataset/museums-in-italy', True )
         else:
-            print "Terminated"
+            print "not yet implemented. terminating"
+
+    # close communication with the database
+    cur.close()
+    conn.close()
 
 # -----------------
 #
