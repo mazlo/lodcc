0bb00b5774fd9af16c31ae319584a2e836be1318
Matthäus Zloch <matthaeus.zloch@gmail.com>
2017-12-22 14:29:37 +0100

    move helper tests to test_lodcchelpers

--- test_lodcchelpers.py
+++ test_lodcchelpers.py
@@ -18,3 +18,26 @@ class LodccHelperTestCase( unittest.TestCase ):
         self.assertEqual( lodcc.get_file_mediatype( 'filename.rdf' ), 'rdf' )
         self.assertEqual( lodcc.get_file_mediatype( 'filename.tgz' ), 'tgz' )
         self.assertEqual( lodcc.get_file_mediatype( 'filename.tar.gz' ), 'tar.gz' )
+
+    def test_ensure_valid_filename_from_url( self ):
+        
+        basenames = [ 
+            # column1: url, column2: expected filename
+            # easy: filename exists
+            { 'url': 'https://ckannet-storage.commondatastorage.googleapis.com/2014-11-27T14:31:27.350Z/apertium-es-ast-rdf.zip', 'basename': 'apertium-es-ast-rdf.zip' },
+            { 'url': 'https://drive.google.com/file/d/0B8VUbXki5Q0ibEIzbkUxSnQ5Ulk/dump.tar.gz?usp=sharing', 'basename': 'dump.tar.gz' },
+            { 'url': 'http://gilmere.upf.edu/corpus_data/ParoleSimpleOntology/ParoleEntries.owl', 'basename': 'ParoleEntries.owl' },
+            # hard: set filename to dump_...rdf
+            { 'url': 'http://dump.linkedopendata.it/musei', 'basename': 'dump_testfile.rdf' },
+            { 'url': 'http://n-lex.publicdata.eu/resource/export/f/rdfxml?r=http%3A%2F%2Fn-lex.publicdata.eu%2Fgermany%2Fid%2FBJNR036900005', 'basename': 'dump_testfile.rdf' },
+            { 'url': 'http://data.nobelprize.org', 'basename': 'dump_testfile.rdf' },
+            { 'url': 'http://spatial.ucd.ie/lod/osn/data/term/k:waterway/v:river', 'basename': 'dump_testfile.rdf' },
+        ]
+
+        for test in basenames:
+            basename_is = lodcc.ensure_valid_filename_from_url( { 'name': 'testfile' }, test['url'], 'application_rdf_xml' )
+            self.assertEqual( basename_is, test['basename'] )
+    
+    def test_ensure_valid_filename_from_url_None( self ):
+
+        self.assertIsNone( lodcc.ensure_valid_filename_from_url( { 'name': 'testfile' }, None, None ) )
