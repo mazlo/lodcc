95e04e16d29f578f84de773a0e93bf0282516dfb
Matthäus Zloch <matthaeus.zloch@gmail.com>
2017-12-30 22:35:06 +0100

    comments to *.sh files

--- to_ntriples.sh
+++ to_ntriples.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# 
+# given a filename called 'foo.bar', this process will write the data into a file named: 'foo.bar.nt'
+#
+
 INPUT_FORMAT="${2:-rdfxml}"
 FILE=$1
+
 rapper --quiet --input "$INPUT_FORMAT" --output "ntriples" "$FILE" | sort > "$FILE.nt"
