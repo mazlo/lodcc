f5232043b4332960dbfba3cc7ba70cd140303f53
Matthäus Zloch <matthaeus.zloch@gmail.com>
2018-01-04 15:57:26 +0100

    to_ntriples -> reuse existing files when caching is disabled; closes #11

--- to_ntriples.sh
+++ to_ntriples.sh
@@ -4,6 +4,27 @@
 #
 
 INPUT_FORMAT="${2:-rdfxml}"
-FILE=$1
+INPUT=$1
+OUTPUT="$INPUT.nt"
+NO_CACHE=${3:-false}
 
-rapper --ignore-errors --quiet --input "$INPUT_FORMAT" --output "ntriples" "$FILE" > "$FILE.nt"
+file_exists()
+{
+    if [ ! -f "$OUTPUT" ]; then
+        return 1 # exit failure
+    fi
+
+    SIZE=`ls -s "$OUTPUT" | cut -d ' ' -f1`
+    if [[ $NO_CACHE = false && $SIZE > 1000 ]]; then
+        return 0 # exit success
+    fi
+
+    return 1 # exit failure
+}
+
+# do not transform when file exists, i.e. file size > 1000
+if file_exists; then
+    exit 0 # exit success
+fi
+
+rapper --ignore-errors --quiet --input "$INPUT_FORMAT" --output "ntriples" "$INPUT" > "$OUTPUT"
