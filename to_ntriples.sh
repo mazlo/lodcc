a2dddd96410157ab5bbf9552ed652be65ee7fa60
Matthäus Zloch <matthaeus.zloch@gmail.com>
2018-01-09 15:16:11 +0100

    splitted all code in functions

--- to_ntriples.sh
+++ to_ntriples.sh
@@ -3,28 +3,60 @@
 # given a filename called 'foo.bar', this process will write the data into a file named: 'foo.bar.nt'
 #
 
-INPUT_FORMAT="${2:-rdfxml}"
-INPUT=$1
-OUTPUT="$INPUT.nt"
-NO_CACHE=${3:-false}
+FILE_FORMAT="${1:-rdfxml}"
+FILENAME=$2
+FOLDER_DEST=$3 # e.g. dumps/dataset
+XMTYPE=$4 # e.g. .tar.gz
 
-file_exists()
+# 1. bar.nt.tgz -> bar.nt
+# 2. bar.tar.gz -> bar
+# this will be the directory name
+FILENAME_STRIPPED=`echo "$FILENAME" | sed "s/$XMTYPE//"`
+
+do_extract()
 {
-    if [ ! -f "$OUTPUT" ]; then
-        return 1 # exit failure
-    fi
+    # 'tar.gz', 'tar.xz', 'tgz', 'gz', 'zip', 'bz2', 'tar'
+    if  [[ $XMTYPE == '*.tar.gz' ]] || 
+        [[ $XMTYPE == '*.tar.xz']] || 
+        [[ $XMTYPE == '*.tgz' ]] || 
+        [[ $XMTYPE == '*.gz' ]] || 
+        [[ $XMTYPE == '*.zip' ]] ||
+        [[ $XMTYPE == '*.bz2' ]] || 
+        [[ $XMTYPE == '*.tar' ]]; then
 
-    SIZE=`ls -s "$OUTPUT" | cut -d ' ' -f1`
-    if [[ $NO_CACHE = false && $SIZE > 1000 ]]; then
-        return 0 # exit success
+        FOLDER_SRC=`pwd`
+        cd $FOLDER_DEST
+        dtrx --one rename --overwrite $FILENAME
+        cd $FOLDER_SRC
     fi
-
-    return 1 # exit failure
 }
 
-# do not transform when file exists, i.e. file size > 1000
-if file_exists; then
-    exit 0 # exit success
-fi
+do_convert()
+{
+    # convert all files in directory
+    if [[ -d "$FILENAME_STRIPPED" ]]; then
+        for f in $FOLDER_DEST; do
+            rapper --ignore-errors --quiet --input "$FILE_FORMAT" --output "ntriples" "$f" > "$f.nt"
+        done
+    fi
 
-rapper --ignore-errors --quiet --input "$INPUT_FORMAT" --output "ntriples" "$INPUT" > "$OUTPUT"
+    # convert file
+    if [[ -f "$FILENAME_STRIPPED" ]]; then
+        rapper --ignore-errors --quiet --input "$FILE_FORMAT" --output "ntriples" "$FILENAME" > "$FILENAME.nt"
+    fi
+}
+
+do_oneliner()
+{
+    # check if extracted file is directory
+    # if so, create one file from all the files there
+    if [ -d "$FILENAME_STRIPPED" ]; then
+      find "$FILENAME_STRIPPED" -type f -exec cat {} >> "$FILENAME_STRIPPED.tmp"  \; \
+      && rm -rf "$FILENAME_STRIPPED" \
+      && mv "$FILENAME_STRIPPED.tmp" "$FILENAME_STRIPPED"
+    fi
+}
+
+do_extract
+do_convert
+do_oneliner
