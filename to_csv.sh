fab3f37946e0b263b7a42e2d10f125a6eb254964
Matthäus Zloch <matthaeus.zloch@gmail.com>
2018-01-10 11:56:44 +0100

    fix, to_csv -> adjust to recent to_ntriples changes

--- to_csv.sh
+++ to_csv.sh
@@ -4,22 +4,28 @@
 #
 
 FPATH="$1"
-
 # from PATH
 FILENAME=`echo ${FPATH##*/}`
 
-INPUT="$1"
-OUTPUT="$1.csv"
-NO_CACHE=${2:-false}
-INPUT_EXT=${3:-".nt"}
+get_xmtype()
+{
+    for mtype in 'tar.gz' 'tar.xz' 'tgz' 'gz' 'zip' 'bz2' 'tar'; do
+        # ${"foo.bar.tar.gz"%*.tar.gz} returns "foo.bar"
+        # ${"foo.bar.bz2"%*.gz} returns "foo.bar.bz2"
+        if [[ "${FILENAME%*.$mtype}" != "$FILENAME" ]]; then
+            echo $mtype
+            return
+        fi
+    done
+}
 
 file_exists() 
 {
-    if [ ! -f "$OUTPUT" ]; then
+    if [ ! -f "$FPATH_OUTPUT" ]; then
         return 1 # exit failure
     fi
 
-    SIZE=`ls -s "$OUTPUT" | cut -d ' ' -f1`
+    SIZE=`ls -s "$FPATH_OUTPUT" | cut -d ' ' -f1`
     if [[ $NO_CACHE = false && $SIZE > 1000 ]]; then
         return 0 # exit success
     fi
@@ -27,11 +33,27 @@ file_exists()
     return 1 # exit failure
 }
 
-# do not transform when file exists, i.e. file size > 1000
+# 1. dumps/foo/bar.nt.tgz -> dumps/foo/bar.nt
+# 2. dumps/foo/bar.tar.gz -> dumps/foo/bar
+# this will be the directory or filename
+XMTYPE=`get_xmtype`
+
+FPATH_STRIPPED=`echo ${FPATH%*.$XMTYPE}`
+FPATH_OUTPUT="$FPATH_STRIPPED.csv"
+
+NO_CACHE=${2:-false}
+INPUT_EXT=${3:-"nt"}
+
+# we're reading from a file with $INPUT_EXT, so
+# add extension if the file does not have it already
+if [[ "${FPATH_STRIPPED%*.$INPUT_EXT}" == "$FPATH_STRIPPED" ]]; then
+    FPATH_STRIPPED="$FPATH_STRIPPED.$INPUT_EXT"
+fi
+
 if file_exists; then
     exit 0 # exit success
 fi
 
 # paste the content | add a . at the end | as long as there are lines, rewrite their position
-cat "$INPUT" | while read -r s p o; do echo "$s $o {'edge':'$p'}"; done > "$OUTPUT"
+cat "$FPATH_STRIPPED" | while read -r s p o; do echo "$s $o {'edge':'$p'}"; done > "$FPATH_OUTPUT"
 
