1a62368ee7ad7784b66964692d98701cb193886a
Matthäus Zloch <matthaeus.zloch@gmail.com>
2018-02-23 18:04:46 +0100

    ldicthash.py -> do not use threads, single threaded

--- lodcc/ldicthash.py
+++ lodcc/ldicthash.py
@@ -1,113 +1,118 @@
 import argparse
+import logging as log
 import os
 import pickle
 import re
-import threading
 
-def iedgelist_nt( path, sem=threading.Semaphore(1) ):
+def iedgelist_nt( path, offset=1 ):
     """"""
 
-    # can I?
-    with sem:
-        with open( path, 'r' ) as file:
+    dirname = os.path.dirname( path )
+    filename = os.path.basename( path )
+    prefix = re.sub( '.nt$', '', filename )
+
+    with open( path, 'r' ) as file:
             
-            iedgelist = open( 'data.iedgelist.csv', 'w' )
+        iedgelist = open( '%s/%s.%s' % (dirname,prefix,'iedgelist.csv'), 'w' )
+        log.info( 'handling %s', iedgelist.name )
 
-            idx = 1
-            spo_dict = {}
+        idx = offset + 1
+        spo_dict = {}
 
-            print 'start'
-            for line in file:
-                if re.search( '^# ', line ):
-                    continue
+        for line in file:
+            if re.search( '^# ', line ):
+                continue
 
-                spo = re.split( ' ', line )
+            spo = re.split( ' ', line )
 
-                s = spo[0]
-                p = spo[1]
-                o = ' '.join( spo[2:-1] )
+            s = spo[0]
+            p = spo[1]
+            o = ' '.join( spo[2:-1] )
 
-                if s not in spo_dict:
-                    spo_dict[s] = idx
-                    idx += 1
-                if p not in spo_dict:
-                    spo_dict[p] = idx
-                    idx += 1
-                if o not in spo_dict:
-                    spo_dict[o] = idx
-                    idx += 1
+            if s not in spo_dict:
+                spo_dict[s] = idx
+                idx += 1
+            if p not in spo_dict:
+                spo_dict[p] = idx
+                idx += 1
+            if o not in spo_dict:
+                spo_dict[o] = idx
+                idx += 1
 
-                s = spo_dict[s]
-                p = spo_dict[p]
-                o = spo_dict[o]
+            s = spo_dict[s]
+            p = spo_dict[p]
+            o = spo_dict[o]
 
-                iedgelist.write( '%s %s %s\n' % (s,p,o) )
+            iedgelist.write( '%s %s %s\n' % (s,p,o) )
 
-            iedgelist.close()
+        iedgelist.close()
 
-            rev_spo_dict = { v: k for k, v in spo_dict.items() }
-            
-            with open( 'data.iedgelist_dict.pkl', 'w' ) as pkl:
-                print 'dumping reverse dict..'
-                pickle.dump( rev_spo_dict, pkl )
+        rev_spo_dict = { v: k for k, v in spo_dict.items() }
 
-def iedgelist_csv( path, sem=threading.Semaphore(1) ):
+        pkl_filename = '%s/%s.%s' % (dirname,prefix,'iedgelist.pkl')
+        with open( pkl_filename, 'w' ) as pkl:
+            log.info( 'dumping pickle %s', pkl_filename )
+            pickle.dump( rev_spo_dict, pkl )
+
+        return idx
+
+def iedgelist_csv( path, offset=1 ):
     """"""
 
-    with sem:
-        with open( path, 'r' ) as file:
+    with open( path, 'r' ) as file:
             
-            iedgelist = open( 'data.iedgelist.csv', 'w' )
+        iedgelist = open( 'data.iedgelist.csv', 'w' )
 
-            idx = 1
-            spo_dict = {}
+        idx = 1
+        spo_dict = {}
 
-            print 'start'
-            for line in file:
-                if re.search( '^# ', line ):
-                    continue
+        print 'start'
+        for line in file:
+            if re.search( '^# ', line ):
+                continue
 
-                sp=re.split( '{\'edge\':\'', line )
-                so=re.split( ' ', sp[0] )
+            sp=re.split( '{\'edge\':\'', line )
+            so=re.split( ' ', sp[0] )
 
-                s = so[0]
-                p = sp[1][0:-3]
-                o = ' '.join( so[1:-1] )
+            s = so[0]
+            p = sp[1][0:-3]
+            o = ' '.join( so[1:-1] )
 
-                if s not in spo_dict:
-                    spo_dict[s] = idx
-                    idx += 1
-                if p not in spo_dict:
-                    spo_dict[p] = idx
-                    idx += 1
-                if o not in spo_dict:
-                    spo_dict[o] = idx
-                    idx += 1
+            if s not in spo_dict:
+                spo_dict[s] = idx
+                idx += 1
+            if p not in spo_dict:
+                spo_dict[p] = idx
+                idx += 1
+            if o not in spo_dict:
+                spo_dict[o] = idx
+                idx += 1
 
-                s = spo_dict[s]
-                p = spo_dict[p]
-                o = spo_dict[o]
+            s = spo_dict[s]
+            p = spo_dict[p]
+            o = spo_dict[o]
 
-                iedgelist.write( '%s %s %s\n' % (s,p,o) )
+            iedgelist.write( '%s %s %s\n' % (s,p,o) )
 
-            iedgelist.close()
+        iedgelist.close()
 
-            rev_spo_dict = { v: k for k, v in spo_dict.items() }
-            
-            with open( 'data.iedgelist_dict.pkl', 'w' ) as pkl:
-                print 'dumping reverse dict..'
-                pickle.dump( rev_spo_dict, pkl )
+        rev_spo_dict = { v: k for k, v in spo_dict.items() }
+           
+        with open( 'data.iedgelist_dict.pkl', 'w' ) as pkl:
+            print 'dumping reverse dict..'
+            pickle.dump( rev_spo_dict, pkl )
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser( description = 'lodcc iedgelist' )
     parser.add_argument( '--paths', '-p', nargs='*', required = True, help = '' )
     parser.add_argument( '--csv', '-c', action='store_true', help = '' )
+ 
+    log.basicConfig( level = log.INFO, 
+                     format = '[%(asctime)s] - %(levelname)-8s : %(message)s', )
 
     args = vars( parser.parse_args() )
     paths = args['paths']
-    sem = threading.Semaphore( 8 )
-    threads = []
 
     if args['csv']:
         ending = '.csv$'
@@ -124,19 +129,13 @@ if __name__ == '__main__':
             if not re.search( '/$', path ):
                 path = path+'/'
 
+            idx_offset = 1
             for filename in os.listdir( path ):
 
                 if not re.search( ending, filename ):
                     continue
 
-                t = threading.Thread( target = func, name = filename, args = ( path + filename, sem ) )
-                t.start()
-
-                threads.append( t )
-
-            for t in threads:
-                t.join()
-        
+                idx_offset = func( path + filename, idx_offset )
         else:
             # if given path is a file, use it
             func( path )
