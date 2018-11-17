2d1da3e39ab9bad04abb77fef8895771540c13d4
Matthäus Zloch <matthaeus.zloch@gmail.com>
2018-11-17 17:42:44 +0100

    refactor sampling -> multithreaded

--- lodcc/sample_edgelist_weighted.py
+++ lodcc/sample_edgelist_weighted.py
@@ -3,7 +3,44 @@ import logging as log
 import numpy as np
 import os
 import re
+import threading
 
+def sample_edgelist_job( dataset, n, k, sem ):
+    """"""
+
+    with sem:
+        if log:
+            log.info( 'Taking sample ..' )
+
+        sample  = np.random.choice( n, int( n.size*k ), replace=False )
+        sample.sort()
+        
+        sample_dir = os.path.dirname( dataset ) + '-sampled-%s' % k
+
+        # e.g. dumps/core-sampled-0.25 gets creted if not present
+        if not os.path.isdir( sample_dir ):
+            if log:
+                log.info( 'Creating directory ..' )
+            os.mkdir( sample_dir )
+        
+        with open( '%s/data.edgelist.csv' % sample_dir, 'w' ) as dataset_sampled:
+            with open( dataset ) as f:
+                if log:
+                    log.info( 'Sampling edgelist ..' )
+                for i, line in enumerate(f):
+                    for k, line_sample in enumerate(sample):
+                        if line_sample > i:
+                            break
+                        if line_sample < i:
+                            continue
+                        
+                        dataset_sampled.write( line )
+                        sample = sample[k:]
+                        break
+
+        if log:
+            log.info( 'Done' )
+                
 def sample_edgelist( paths, log=None ):
     """"""
 
@@ -33,25 +70,20 @@ def sample_edgelist( paths, log=None ):
         num_lines = sum( 1 for line in open( dataset ) )
         n         = np.arange( num_lines ) + 1
 
-        for k in np.arange( 0.25, 1, 0.25 ):  # [ 0.25, 0.5, 0.75 ]
-            if log:
-                log.info( 'Taking %s sample edgelist..', k )
+        # prepare
+        sem = threading.Semaphore( 10 )
+        threads = []
 
-            sample  = np.random.choice( n, int( n.size*k ), replace=False )
-            sample.sort()
+        for k in np.linspace(0.05, 0.5, num=10):  # e.g. [ 0.25, 0.5, 0.75 ]
 
-            with open( 'data.edgelist.%s.csv' % k, 'w' ) as dataset_sampled:
-                with open( dataset ) as f:
-                    for i, line in enumerate(f):
-                        for k, line_sample in enumerate(sample):
-                            if line_sample > i:
-                                break
-                            if line_sample < i:
-                                continue
+            t = threading.Thread( target = sample_edgelist_job, name = '%s[%s]' % ( os.path.dirname(dataset), k ), args = ( dataset, n, k, sem ) )
+            t.start()
 
-                            dataset_sampled.write( line )
-                            sample = sample[k:]
-                            break
+            threads.append( t )
+
+        # wait for all threads to finish
+        for t in threads:
+            t.join()
 
 #
 if __name__ == '__main__':
@@ -61,7 +93,7 @@ if __name__ == '__main__':
 
     log.basicConfig(
             level = log.INFO, 
-            format = '[%(asctime)s] - %(levelname)-8s : %(message)s', )
+            format = '[%(asctime)s] - %(levelname)-8s : %(threadName)s: %(message)s', )
 
     args = vars( parser.parse_args() )
     paths = args['paths']
