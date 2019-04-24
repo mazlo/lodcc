53f31ad319d95041c3317fc6b7075af155b3739e
Matthäus Zloch <matthaeus.zloch@gmail.com>
2019-04-24 09:49:56 +0200

    completely rewritten bfv_from_file
    - changed strategy how to match remaining hashes
    - only uses the hashes passed to program, no database
    - use terms in accordance to main program

--- lodcc/bfv_from_file.py
+++ lodcc/bfv_from_file.py
@@ -8,61 +8,36 @@ try:
 except:
     print 'psycogp2 could not be found'
 import re
-import threading
 import xxhash as xh
 
 from ldicthash import parse_spo
-
-def find_path( dataset ):
-    """"""
-
-    if dataset['path_edgelist']:
-        return os.path.dirname( dataset['path_edgelist'] )
-    elif dataset['path_graph_gt']:
-        return os.path.dirname( dataset['path_graph_gt'] )
-
-    return None
 
 def find_nt_files( path ):
     """"""
 
     return [ nt for nt in os.listdir( path ) if re.search( '\.nt$', nt ) ]
 
-def save_hash( dataset, column, uri ):
+def bf_necessary( vertices_map ):
     """"""
 
-    sql = 'UPDATE stats_graph SET '+ column +'_uri=%(uri)s WHERE id=%(id)s'
-    val_dict = { 'uri': uri, 'id': dataset['id'] }
+    if len( vertices_map ) == 0:
+        return {}
 
-    cur = conn.cursor()
-    cur.execute( sql, val_dict )
-    conn.commit()
-    cur.close()
-    
-    log.debug( sql % val_dict )
+    # vertices_map = { 'v1': ( 'value', False ) }
+    # False, in case the value still has to be dereferenced
 
-def get_hashes_to_find( dataset, col_names ):
-    """"""
+    return dict( { v[0]:v for k,v in vertices_map.items() if type(v) == tuple and not v[1] } )
 
-    hashes_to_find = {} 
-    for name in col_names:
-        hashv = dataset[name]
-        if not hashv:
-            continue
-
-        if hashv in hashes_to_find:
-            hashes_to_find[hashv].append( name )
-        else:
-            hashes_to_find[hashv] = [name]
-
-    return hashes_to_find
-
-def find_vertices( in_file, dataset, hashes_to_find ):
+def find_vertices( in_file, dataset, vertices_map ):
     """"""
 
     if not in_file:
         log.error( 'Exiting because of previrous errors' )
-        return
+        return vertices_map
+
+    left_to_bf = bf_necessary( vertices_map )
+    if len( left_to_bf ) == 0:
+        return vertices_map
 
     with open( in_file, 'r' ) as openedfile:
         for line in openedfile:
@@ -71,56 +46,61 @@ def find_vertices( in_file, dataset, hashes_to_find ):
 
             sh = xh.xxh64( s ).hexdigest()
             oh = xh.xxh64( o ).hexdigest()
-            
-            if sh in hashes_to_find:
-                cols = hashes_to_find[sh]
-                for col in cols:
-                    save_hash( dataset, col, s )
-                
-                del hashes_to_find[sh]
 
-            if oh in hashes_to_find:
-                cols = hashes_to_find[oh]
-                for col in cols:
-                    save_hash( dataset, col, o )
-                
-                del hashes_to_find[oh]
+            if sh in left_to_bf:
+                vertices_map[sh] = (s,True)
 
-            # checked, over?
-            if len( hashes_to_find ) == 0:
+            if ph in left_to_bf:
+                vertices_map[ph] = (p,True)
+
+            if oh in left_to_bf:
+                vertices_map[oh] = (o,True)
+
+            # over?
+            left_to_bf = bf_necessary( vertices_map )
+            if len( left_to_bf ) == 0:
                 break   # done
 
-def job_find_vertices( dataset, sem ):
+        return vertices_map
+
+def job_find_vertices( dataset, vertices ):
     """"""
 
-    # can I?
-    with sem:
-        path = find_path( dataset )
-        files = find_nt_files( path )
+    if not os.path.isdir( dataset ):
+        log.error( 'Dataset %s is not a directory.' % ( dataset ) )
+        return
 
-        if len( files ) == 0:
-            log.warning( 'No nt-file found for dataset %s', dataset['name']  )
-            return
+    files = find_nt_files( dataset )
 
-        col_names = ['max_degree_vertex', 'max_pagerank_vertex', 'max_in_degree_vertex', 'max_out_degree_vertex'] #,'pseudo_diameter_src_vertex', 'pseudo_diameter_trg_vertex']
-        hashes_to_find = get_hashes_to_find( dataset, col_names )
+    if len( files ) == 0:
+        log.warning( 'No nt-file found for dataset %s', dataset  )
+        return
 
-        for file in files:
-            find_vertices( '/'.join( [path,file] ), dataset, hashes_to_find )
+    if type( vertices ) == list:
+        # before: vertices = [ 'v1', 'v2', ... ]
+        # after: vertices = { 'v1': ('v1', False), 'v2': ('v2', False), ... }
+        vertices = dict( { v:(v,False) for v in vertices } )
 
-            # checked, over?
-            if len( hashes_to_find ) == 0:
-                break   # done
+    log.debug( 'Scanning %s files (in %s)' % ( len(files), files ) )
+    for file in files:
+        vertices = find_vertices( '/'.join( [dataset,file] ), dataset, vertices )
 
-        log.info( 'Done' )
+        # over?
+        left_to_bf = bf_necessary( vertices )
+        if len( left_to_bf ) == 0:
+            break   # done
+
+    # before: vertices = { 'v1': ('v1', False), 'v2': ('v2', False), ... }
+    # after: vertices = { 'v1': 'v1', 'v2': 'v2', ... }
+    return dict( { k:v[0] for k,v in vertices.items() } )
 
 if __name__ == '__main__':
 
     # configure args parser
     parser = argparse.ArgumentParser( description = 'lodcc - find xxhash from original data' )
-    parser.add_argument( '--use-datasets', '-du', nargs='*', required=True, help = '' )
+    parser.add_argument( '--from-file', '-ffl', action = "append", help = '', nargs = '*')
     parser.add_argument( '--processes', '-dp', type=int, required=False, default=1, help = '' )
-    parser.add_argument( '--vertices', '-n', nargs='*', required=False, help = '' )
+    parser.add_argument( '--vertices', '-n', nargs='*', required=True, help = '' )
 
     parser.add_argument( '--log-debug', action='store_true', help = '' )
 
@@ -134,73 +114,12 @@ if __name__ == '__main__':
     # configure log
     log.basicConfig( level = level, format = '[%(asctime)s] - %(levelname)-8s : %(threadName)s: %(message)s', )
 
-    # read all properties in file into args-dict
-    if os.path.isfile( 'db.properties' ):
-        with open( 'db.properties', 'rt' ) as f:
-            args = dict( ( key.replace( '.', '-' ), value ) for key, value in ( re.split( "=", option ) for option in ( line.strip() for line in f ) ) )
-    else:
-        log.error( 'Please verify your settings in db.properties (file exists?)' )
-        sys.exit()
-
     z = vars( parser.parse_args() ).copy()
     z.update( args )
     args = z
 
-    # connect to an existing database
-    conn = psycopg2.connect( host=args['db-host'], dbname=args['db-dbname'], user=args['db-user'], password=args['db-password'] )
-    conn.set_session( autocommit=True )
-
-    # check database connection
-    try:
-        cur = conn.cursor()
-        cur.execute( "SELECT 1;" )
-        result = cur.fetchall()
-        cur.close()
-
-        log.debug( 'Database ready to query execution' )
-    except:
-        log.error( 'Database not ready for query execution. %s', sys.exc_info()[0] )
-        raise 
-
-    # respect --use-datasets argument
-    if not args['use_datasets'] or len( args['use_datasets'] ) == 0:
-        log.error( '--use-datasets not provided' )
-        sys.exit()
-
-    names_query = '( ' + ' OR '.join( 'name = %s' for ds in args['use_datasets'] ) + ' )'
-    names = tuple( args['use_datasets'] )
-
-    sql = 'SELECT id,name,max_degree_vertex,max_pagerank_vertex,max_in_degree_vertex,max_out_degree_vertex,pseudo_diameter_src_vertex,pseudo_diameter_trg_vertex,path_edgelist,path_graph_gt FROM stats_graph WHERE '+ names_query +' AND (max_degree_vertex IS NOT NULL OR max_pagerank_vertex IS NOT NULL) ORDER BY id'
-
-    # get datasets from database
-    cur = conn.cursor( cursor_factory=psycopg2.extras.DictCursor )
-    cur.execute( sql, names )
-
-    if cur.rowcount == 0:
-        log.warning( '--use-datasets specified but empty result set' )
-        sys.exit()
-
-    datasets = cur.fetchall()
-
-    if os.path.isfile( 'found_hashes.pkl' ):
-        # load already found hashes
-        pkl_file = open( 'found_hashes.pkl', 'rb')
-        global_hashes = pickle.load( pkl_file )
-    else:
-        global_hashes = {}
-
-    # setup threading
-    sem = threading.Semaphore( args['processes'] )
-    threads = []
+    datasets = args['from_file']        # argparse returns [[..], [..],..]
+    datasets = map( lambda d: 'dumps/%s' % d[0], datasets )
 
     for dataset in datasets:
-
-        t = threading.Thread( target = job_find_vertices, name = dataset['name'], args = ( dataset, sem ) )
-        t.start()
-        threads.append(t)
-
-    for t in threads:
-        t.join()
-    
-    pkl_file = open( 'found_hashes.pkl', 'wb')
-    pickle.dump( global_hashes, pkl_file )
+        print( job_find_vertices( dataset, args['vertices'] ) )
