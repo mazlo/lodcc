d725770514415c171a89d66a63235663372ee0be
Matthäus Zloch <matthaeus.zloch@gmail.com>
2019-07-04 13:57:32 +0200

    change impl. of subj. out-degrees, now with numpy

--- graph/metrics/fernandez_et_al/subject_out_degrees.py
+++ graph/metrics/fernandez_et_al/subject_out_degrees.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 # SUBJECT OUT-DEGREES
 
@@ -6,29 +7,36 @@ def partial_out_degree( D, stats ):
     """"""
 
     # the number of triples of G, in which s occurs as subject and p as predicate
-    ## e.g. l = ['/John_foaf:mbox', '/John_foaf:mbox', '/John_rdf:type', '/John_ex:birthPlace', '/Rome_foaf:name', '/Giacomo_ex:areaOfWork', '/Piero_ex:areaOfWork']
-    l = [ (D.vp.name[s],D.ep.c0[p]) for s in D.vertices() if D.vp.subject[s] for p in s.out_edges() ]
+    ## e.g. l = [ ['/John','foaf:mbox'], ['/John','foaf:mbox'], ['/John','rdf:type'], ['/John','ex:birthPlace'], ['/Rome', 'foaf:name'], ['/Giacomo', 'ex:areaOfWork'], ['/Piero', 'ex:areaOfWork'] ]
+    l = list( zip( 
+            D.get_edges()[:,0], 
+            D.ep.c0.get_2d_array( pos=[0])[0] ) )
+
     _, l = np.unique( l, return_counts=True, axis=0 )
-    print( "(4) partial out-degree. max: %s, mean: %s" % ( np.max( l ), np.mean( l ) ) )
+    print( "(4) partial out-degree. max: %s, mean: %f" % ( np.max( l ), np.mean( l ) ) )
 
 def labelled_out_degree( D, stats ):
     """"""
 
     # the number of different predicates (labels) of G with which s is related as a subject
-    ## e.g. l = [ ['foaf:mbox', 'foaf:mbox', 'rdf:type', 'ex:birthPlace'], ['foaf:name'], ['ex:areaOfWork'], ['ex:areaOfWork'] ]
-    l = np.array( [ len( { D.ep.c0[p] for p in s.out_edges() } ) 
-                   for s in D.vertices() 
-                   if D.vp.subject[s] ] )
+    l = zip( 
+        D.get_edges()[:,0], 
+        D.ep.c0.get_2d_array([0])[0] )
+    df = pd.DataFrame( data=list(l), index=np.arange(0, D.get_edges().shape[0]), columns=np.arange(0, D.get_edges().shape[1]-1) )
+
+    l = df.groupby(0).nunique()[1]
     print( "(5) labelled out-degree. max: %s, mean: %s" % ( l.max(), l.mean() ) )
 
 def direct_out_degree( D, stats ):
     """"""
 
     # the number of different objects of G with which s is related as a subject
-    ## e.g. l = [ ['john@example.org', 'john@doe.org', '/Researcher', '/Rome'], ['"Roma"@it'], ['/Rome'], ['/Rome'] ]
-    l = np.array( [ len( { D.vp.name[p.target()] for p in s.out_edges() } ) 
-                   for s in D.vertices() 
-                   if D.vp.subject[s] ] )
+    df = pd.DataFrame( 
+        data=D.get_edges(), 
+        index=np.arange( 0, D.get_edges().shape[0] ), 
+        columns=np.arange(0, D.get_edges().shape[1]) )
+
+    l = df.groupby(0).nunique()[1]
     print( "(6) direct out-degree. max: %s, mean: %s" % ( l.max(), l.mean() ) )
 
 all = [ partial_out_degree, labelled_out_degree, direct_out_degree ]
