7d565796615b9f3086f7578b55d54ef65c27aaee
Matthäus Zloch <matthaeus.zloch@gmail.com>
2019-07-08 11:00:32 +0200

    add common ratio metric: subject object ratio

--- graph/metrics/fernandez_et_al/all.py
+++ graph/metrics/fernandez_et_al/all.py
@@ -1,5 +1,6 @@
 import graph.metrics.fernandez_et_al.subject_out_degrees as subj_out_deg
 import graph.metrics.fernandez_et_al.object_in_degrees as obj_in_deg
 import graph.metrics.fernandez_et_al.predicate_degrees as pred_deg
+import graph.metrics.fernandez_et_al.common_ratios as ratios
 
-all = subj_out_deg.all + obj_in_deg.all + pred_deg.all
+all = subj_out_deg.all + obj_in_deg.all + pred_deg.all + ratios.all
---
+++ graph/metrics/fernandez_et_al/common_ratios.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def subject_object_ratio( D, stats, edge_labels=None ):
+    """"""
+
+    # the number of elements acting both as subject and objects among all subjects and objects
+    nom_intersection = set( D.get_edges()[:,0] ) & set( D.get_edges()[:,1] )
+    denom_union = set( D.get_edges()[:,0] ) | set( D.get_edges()[:,1] )
+    print( "(Eq.12) subject-object ratio \\alpha_{s-o}(G): %f" % (float(len(nom_intersection)) / len(denom_union)) )
+
+def subject_predicate_ratio( D, stats, edge_labels=np.empty(0) ):
+    """"""
+
+    # TODO because this is a costly computation
+
+def predicate_object_ratio( D, stats, edge_labels=np.empty(0) ):
+    """"""
+
+    # TODO because this is a costly computation
+
+all = [ subject_object_ratio ]
