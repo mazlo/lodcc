e204cfbb1f31490ae2aad30ed07e4eaa79b5da5c
Matthäus Zloch <matthaeus.zloch@gmail.com>
2019-07-08 18:08:27 +0200

    add ratio of predicate lists metric

--- graph/metrics/fernandez_et_al/all.py
+++ graph/metrics/fernandez_et_al/all.py
@@ -2,5 +2,6 @@ import graph.metrics.fernandez_et_al.subject_out_degrees as subj_out_deg
 import graph.metrics.fernandez_et_al.object_in_degrees as obj_in_deg
 import graph.metrics.fernandez_et_al.predicate_degrees as pred_deg
 import graph.metrics.fernandez_et_al.common_ratios as ratios
+import graph.metrics.fernandez_et_al.predicate_lists as pred_lists
 
-all = subj_out_deg.all + obj_in_deg.all + pred_deg.all + ratios.all
+all = subj_out_deg.all + obj_in_deg.all + pred_deg.all + ratios.all + pred_lists.all
---
+++ graph/metrics/fernandez_et_al/predicate_lists.py
@@ -0,0 +1,25 @@
+from graph_tool import GraphView
+import numpy as np
+import pandas as pd
+
+def ratio_repeated_predicate_lists( D, stats, edge_labels=np.empty(0) ):
+    """"""
+
+    if edge_labels.size == 0:
+        edge_labels = D.ep.c0.get_2d_array([0])[0]
+
+    S = GraphView( D, vfilt=D.get_edges()[:,0] )
+
+    # .. is defined as the ratio of repeated predicate lists from the total lists in the graph G
+    l = zip( D.get_edges()[:,0], edge_labels )
+
+    df = pd.DataFrame( 
+        data=list(l), 
+        index=np.arange(0, D.get_edges().shape[0]), 
+        columns=np.arange(0, D.get_edges().shape[1]-1) )
+
+    L_G = df.groupby(0)[1].apply(list).apply(tuple).apply(hash).to_frame().reset_index()
+    L_G = L_G.groupby(1).count()
+    print( "(Eq.17) ratio of repeated predicate lists r_L(G): %f" % (1 - ( L_G.size / S.num_vertices() )) )
+
+all = [ ratio_repeated_predicate_lists ]
