8dae4acb690700c8fefe72fd0f4f75c68b23f100
Matthäus Zloch <matthaeus.zloch@gmail.com>
2019-08-07 11:39:46 +0200

    fix, predicate out-degree, use pandas

--- graph/metrics/fernandez_et_al/predicate_degrees.py
+++ graph/metrics/fernandez_et_al/predicate_degrees.py
@@ -40,13 +40,17 @@ def predicate_out_degree( D, stats, edge_labels=np.empty(0), print_stats=False )
         edge_labels = [ D.ep.c0[p] for p in D.get_edges() ]
 
     # the number of different objects of G with which p is related as a predicate
-    l = list( zip( edge_labels, D.get_edges()[:,1] ) )
-    _, l = np.unique( l, return_counts=True, axis=0 )
+     df = pd.DataFrame( 
+        data=list( zip (preds, D.get_edges()[:,1] ) ), 
+        index=np.arange( 0, D.get_edges().shape[0] ), 
+        columns=np.arange( 0, D.get_edges().shape[1] ) )
+
+    df = df.groupby(0).nunique()[1]
 
     if print_stats:
-        print( "(Eq.11) predicate out-degree deg^{-}_P(p). max: %s, mean: %f" % ( np.max(l), np.mean(l) ) )
+        print( "(Eq.11) predicate out-degree deg^{-}_P(p). max: %s, mean: %f" % ( df.max(), df.mean() ) )
 
-    stats['max_predicate_out_degree'], stats['mean_predicate_out_degree'] = np.max(l), np.mean(l)
+    stats['max_predicate_out_degree'], stats['mean_predicate_out_degree'] = df.max(), df.mean()
 
 METRICS = [ predicate_degree, predicate_in_degree, predicate_out_degree ]
 LABELS  = [ 'max_predicate_degree', 'mean_predicate_degree', 'max_predicate_in_degree', 'mean_predicate_in_degree', 'max_predicate_out_degree', 'mean_predicate_out_degree' ]
