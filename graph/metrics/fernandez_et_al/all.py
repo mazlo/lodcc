41690fad6c0baf3598fab4e274202737125c93b0
Matthäus Zloch <matthaeus.zloch@gmail.com>
2019-08-12 15:52:54 +0200

    use more speaking names for imports

--- graph/metrics/fernandez_et_al/all.py
+++ graph/metrics/fernandez_et_al/all.py
@@ -1,17 +1,16 @@
-import graph.metrics.fernandez_et_al.subject_out_degrees as subj_out_deg
+import graph.metrics.fernandez_et_al.subject_out_degrees as subject_out_degrees
 from graph.metrics.fernandez_et_al.subject_out_degrees import *
-import graph.metrics.fernandez_et_al.object_in_degrees as obj_in_deg
-from graph.metrics.fernandez_et_al.object_in_degrees import *
+import graph.metrics.fernandez_et_al.object_in_degrees as object_in_degrees
 import graph.metrics.fernandez_et_al.predicate_degrees as predicate_degrees
 import graph.metrics.fernandez_et_al.common_ratios as ratios
-import graph.metrics.fernandez_et_al.predicate_lists as pred_lists
+import graph.metrics.fernandez_et_al.predicate_lists as predicate_lists
 from graph.metrics.fernandez_et_al.predicate_lists import *
 import graph.metrics.fernandez_et_al.typed_subjects_objects as typed_so
 
-all    = subj_out_deg.METRICS + obj_in_deg.METRICS + pred_deg.METRICS + ratios.METRICS + pred_lists.METRICS + typed_so.METRICS
-LABELS = subj_out_deg.LABELS + obj_in_deg.LABELS + pred_deg.LABELS + ratios.LABELS + pred_lists.LABELS + typed_so.LABELS
+all    = subject_out_degrees.METRICS + object_in_degrees.METRICS + predicate_degrees.METRICS + ratios.METRICS + predicate_lists.METRICS + typed_so.METRICS
+LABELS = subject_out_degrees.LABELS + object_in_degrees.LABELS + predicate_degrees.LABELS + ratios.LABELS + predicate_lists.LABELS + typed_so.LABELS
 SETS   = {}
-SETS.update( subj_out_deg.METRICS_SET )
-SETS.update( obj_in_deg.METRICS_SET )
-SETS.update( pred_lists.METRICS_SET )
+SETS.update( subject_out_degrees.METRICS_SET )
+SETS.update( object_in_degrees.METRICS_SET )
 SETS.update( predicate_degrees.METRICS_SET )
+SETS.update( predicate_lists.METRICS_SET )
