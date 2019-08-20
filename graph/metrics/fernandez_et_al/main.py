a2916485a0f0a22992e3abc803f324df8fb75e6b
Matthäus Zloch <matthaeus.zloch@gmail.com>
2019-08-20 08:57:13 +0200

    restructure, compute graph partition once, in-degrees

--- graph/metrics/fernandez_et_al/main.py
+++ graph/metrics/fernandez_et_al/main.py
@@ -75,10 +75,19 @@ def graph_analyze_on_partitions( dataset, D, features, stats ):
                 # this should add up all the values we need later when computing the metric
                 data[feature] = getattr( metrics, 'collect_'+ feature.__name__ )( S_G_s, edge_labels, data[feature], {}, args['print_stats'] )
 
-        # compute metric from individual partitions
-        getattr( metrics, 'reduce_'+ feature.__name__ )( data, D, S_G, stats )
+        for feature in feature_subset:
+            # compute metric from individual partitions
+            getattr( metrics, 'reduce_'+ feature.__name__ )( data[feature], D, S_G, stats )
 
-    elif feature in metrics.SETS['OBJECT_IN_DEGREES']:
+    if args['from_db']:
+        db.save_stats( dataset, stats )
+
+    # collect features that require in-degree filtering
+    feature_subset = [ ftr for ftr in features if ftr in metrics.SETS['OBJECT_IN_DEGREES'] ]
+
+    if len( feature_subset ) > 0:
+        log.info( 'Computing features %s on %s partitions of the DiGraph' % ( ', '.join( [ f.__name__ for f in feature_subset ] ), NO_PARTITIONS ) )
+        
         # filter the graph for objects, vertices with in-degree > 0
         O_G = GraphView( D, vfilt=lambda v:v.in_degree() > 0 )
 
@@ -86,40 +95,21 @@ def graph_analyze_on_partitions( dataset, D, features, stats ):
         # will result in this: [ [0,..,759], [760,.., 1519], .., [6840,7599] ]
         partitions = np.array_split( O_G.get_vertices(), NO_PARTITIONS )
 
-        data = None
+        # init data dictionary
+        data = dict( [ (feature,None) for feature in feature_subset ] )
         for o_idx in np.arange( NO_PARTITIONS ):
             # now, we filter out those edges with source vertices from the current partition
             O_G_s = GraphView( D, efilt=np.isin( D.get_edges()[:,1], partitions[o_idx] ) )
             edge_labels = np.array( [ O_G_s.ep.c0[p] for p in O_G_s.edges() ] )
 
-            # this should add up all the values we need later when computing the metric
-            data = metrics.object_in_degrees.collect_metric( feature, O_G_s, edge_labels, data, {}, args['print_stats'] )
+            for feature in feature_subset:
+                # this should add up all the values we need later when computing the metric
+                data[feature] = metrics.object_in_degrees.collect_metric( feature, O_G_s, edge_labels, data[feature], {}, args['print_stats'] )
 
-        # compute metric from individual partitions
-        metrics.object_in_degrees.reduce_metric( data, stats, 'max_'+ feature.__name__, 'mean_'+ feature.__name__ )
-
-    elif feature in metrics.SETS['PREDICATE_DEGREES']:
-        # we first compute a unique set of predicates
-        edge_labels = np.array( [D.ep.c0[p] for p in D.edges() ] )
-        # and split up all predicates into X partitions. 
-        partitions = np.array_split( np.unique( edge_labels ), NO_PARTITIONS )
-
-        data = None
-        for p_idx in np.arange( NO_PARTITIONS ):
-            # now, we filter all edges with labels from the corresponding partition 
-            P_G_s = GraphView( D, efilt=np.isin( edge_labels, partitions[p_idx] ) )
-            # and use the edge labels from the current GraphView for the computation of the feature
-            edge_labels_subgraph = np.array( [ P_G_s.ep.c0[p] for p in P_G_s.edges() ] )
-
-            # this should add up all the values we need later when computing the metric
-            data = metrics.predicate_degrees.collect_metric( feature, P_G_s, edge_labels_subgraph, data, {}, args['print_stats'] )
-
-        # compute metric from individual partitions
-        metrics.predicate_degrees.reduce_metric( data, stats, 'max_'+ feature.__name__, 'mean_'+ feature.__name__ )
         for feature in feature_subset:
             # compute metric from individual partitions
-            getattr( metrics, 'reduce_'+ feature.__name__ )( data[feature], D, S_G, stats )
-
+            metrics.object_in_degrees.reduce_metric( data[feature], stats, 'max_'+ feature.__name__, 'mean_'+ feature.__name__ )
+    
     if args['from_db']:
         db.save_stats( dataset, stats )
 
