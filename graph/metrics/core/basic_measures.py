228fe49a6b656d52f440654b0cac70de14fae095
Matthäus Zloch <matthaeus.zloch@gmail.com>
2020-05-22 18:38:48 +0200

    fix, adjust, cleanup core.basic_measures

--- graph/metrics/core/basic_measures.py
+++ graph/metrics/core/basic_measures.py
@@ -1,8 +1,15 @@
-def fs_digraph_using_basic_properties( D, stats ):
+import logging
+
+from graph_tool import GraphView
+from graph_tool.stats import label_parallel_edges
+
+log = logging.getLogger( __name__ )
+
+def fs_digraph_using_basic_properties( D, stats, options={ 'features': None } ):
     """"""
 
     # at least one of these features needed to continue
-    if len([f for f in ['degree','parallel_edges','fill'] if f in args['features']]) == 0:
+    if len([f for f in ['degree','parallel_edges','fill'] if f in options['features']]) == 0:
         return
 
     # feature: order
@@ -17,16 +24,16 @@ def fs_digraph_using_basic_properties( D, stats ):
     stats['m']=num_edges
 
     # feature: avg_degree
-    if 'degree' in args['features']:
+    if 'degree' in options['features']:
         stats['avg_degree']=float( 2*num_edges ) / num_vertices
         log.debug( 'done avg_degree' )
     
     # feature: fill_overall
-    if 'fill' in args['features']:
+    if 'fill' in options['features']:
         stats['fill_overall']=float( num_edges ) / ( num_vertices * num_vertices )
         log.debug( 'done fill_overall' )
 
-    if 'parallel_edges' in args['features'] or 'fill' in args['features']:
+    if 'parallel_edges' in options['features'] or 'fill' in options['features']:
         eprop = label_parallel_edges( D, mark_only=True )
         PE = GraphView( D, efilt=eprop )
         num_edges_PE = PE.num_edges()
@@ -34,11 +41,11 @@ def fs_digraph_using_basic_properties( D, stats ):
         stats['m_unique']=num_edges - num_edges_PE
 
         # feature: parallel_edges
-        if 'parallel_edges' in args['features']:
+        if 'parallel_edges' in options['features']:
             stats['parallel_edges']=num_edges_PE
             log.debug( 'done parallel_edges' )
 
         # feature: fill
-        if 'fill' in args['features']:
+        if 'fill' in options['features']:
             stats['fill']=float( num_edges - num_edges_PE ) / ( num_vertices * num_vertices )
             log.debug( 'done fill' )
