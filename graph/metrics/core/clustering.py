b702e13df44a400a2bfd301f9e90f20ad7fc7c7f
Matthäus Zloch <matthaeus.zloch@gmail.com>
2020-05-22 21:15:59 +0200

    fix, adjust, cleanup core.clustering

--- graph/metrics/core/clustering.py
+++ graph/metrics/core/clustering.py
@@ -1,26 +1,26 @@
+import logging 
 
-def f_avg_shortest_path( U, stats, sem ):
-    # can I?
-    with sem:
-        stats['avg_shortest_path']=nx.average_shortest_path_length(U)
-        log.debug( 'done avg_shortest_path' )
+from graph_tool.clustering import global_clustering, local_clustering
+from graph_tool.stats import vertex_average
 
-def f_global_clustering( U, stats ):
+log = logging.getLogger( __name__ )
+
+def f_global_clustering( U, stats, options={ 'features': [], 'skip_features': [] } ):
     """"""
 
-    if 'global_clustering' in args['skip_features'] or not 'global_clustering' in args['features']:
+    if not 'global_clustering' in options['features'] or ('skip_features' in options and 'global_clustering' in options['skip_features']):
         log.debug( 'Skipping global_clustering' )
         return
 
     stats['global_clustering']=global_clustering(U)[0]
     log.debug( 'done global_clustering' )
 
-def f_avg_clustering( D, stats ):
+def f_local_clustering( D, stats, options={ 'features': [], 'skip_features': [] } ):
     """"""
     
-    if 'local_clustering' in args['skip_features'] or not 'local_clustering' in args['features']:
-        log.debug( 'Skipping avg_clustering' )
+    if not 'local_clustering' in options['features'] or ('skip_features' in options and 'local_clustering' in options['skip_features']):
+        log.debug( 'Skipping local_clustering' )
         return
 
     stats['avg_clustering']=vertex_average(D, local_clustering(D))[0]
-    log.debug( 'done avg_clustering' )
+    log.debug( 'done local_clustering' )
