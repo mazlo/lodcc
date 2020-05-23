da2a0e64686cb1e9eaee97656321fc61a8900f6e
Matthäus Zloch <matthaeus.zloch@gmail.com>
2020-05-23 17:06:21 +0200

    add constants.measures with predefined sets

---
+++ constants/measures.py
@@ -0,0 +1,3 @@
+BASIC      = ['degree','parallel_edges','fill','h_index','reciprocity','centralization','diameter']
+ADVANCED   = ['gini','powerlaw','pagerank']
+MORE_ADVANCED = ['global_clustering','local_clustering']
